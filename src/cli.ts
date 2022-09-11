bc7f144cfd4d5a317aee0f8dd9eac1ef7c69642a
kamataryo <6292312+kamataryo@users.noreply.github.com>
2022-09-11 22:18:19 +0000

    Deploying to latest from @ geolonia/normalize-any-latlng@180c96f1518c6d92c96c8e20ee8ac9645f3bab5a 🚀

---
+++ src/cli.ts
@@ -0,0 +1,11 @@
+import { normalize } from './index'
+
+const latlngStr = process.argv[2]
+if(!latlngStr) {
+  process.stderr.write(`Invalid LatLng string: ${latlngStr}\n`)
+  process.exit(1)
+} else {
+  const [{lat, lng}] = normalize(latlngStr)
+  process.stdout.write(`${lat} ${lng}\n`)
+  process.exit(0)
+}
